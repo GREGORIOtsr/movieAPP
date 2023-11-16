9dc49d2f0a850dbad149b92bacde235b0e35581f
GREGORIOtsr <gralfonsotr@gmail.com>
2023-11-16 16:03:29 +0100

    created sequelize schemas and controllers/elephantsql connection/email regex

---
+++ src/controllers/user_favorites.controller.js
@@ -0,0 +1,44 @@
+const Users = require('../schemas/sql.users.schema');
+const User_favorites = require('../schemas/sql.user_favorites.schema');
+
+const getFavs = async (req, res) => {
+    try {
+        const user = await Users.findOne({ where: {email: req.body.email} })
+        const movies = await User_favorites.findAll({ where: {user_id: user.id}});
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(400).json({message: `ERROR: ${error.stack}`});
+    }
+};
+
+const createFav = async (req, res) => {
+    try {
+        const user = await Users.findOne({ where: {email: req.body.email} })
+        const newFav = await User_favorites.create({
+            user_id: user.id,
+            movie_id: req.body.movie_id
+        });
+        res.status(201).json(newFav);
+    } catch (error) {
+        
+    }
+};
+
+const deleteFav = async (req, res) => {
+    try {
+        const user = await Users.findOne({ where: {email: req.body.email} })
+        const movie = await User_favorites.destroy({ where: {
+            user_id: user.id,
+            movie_id: req.body.movie_id
+        }});
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(400).json({message: `ERROR: ${error.stack}`});
+    }
+};
+
+const controllers = {
+    getFavs,
+    createFav,
+    deleteFav
+}
---
+++ src/controllers/users.controller.js
@@ -0,0 +1,51 @@
+const Users = require('../schemas/sql.users.schema');
+const User_favorites = require('../schemas/sql.user_favorites.schema');
+
+const getUser = async (req, res) => {
+    try {
+        const user = await Users.findOne({ where: {email: req.body.email} });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({message: `ERROR: ${error.stack}`});
+    }
+};
+
+const createUser = async (req, res) => {
+    try {
+        const user = await Users.create({email: req.body.email});
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({message: `ERROR: ${error.stack}`});
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await Users.update({email: req.body.newEmail}, {
+            where: {
+                email: req.body.email
+            }
+        });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({message: `ERROR: ${error.stack}`});
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await Users.destroy({ where: {email: req.body.email} });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({message: `ERROR: ${error.stack}`});
+    }
+};
+
+const controllers = {
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+};
+
+module.exports = controllers;
