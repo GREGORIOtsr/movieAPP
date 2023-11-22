67da261ef0adc25963796184c2f4393da633369d
GREGORIOtsr <gralfonsotr@gmail.com>
2023-11-22 12:48:20 +0100

    JWT and Google sign up and login/strategy config/auth routes

---
+++ src/config/jwt.config.js
@@ -0,0 +1,30 @@
+const passportJWT = require("passport-jwt");
+const User = require("../schemas/mongo.users.schema");
+require("dotenv").config();
+
+// Passport & JWT config
+const { Strategy: JWTStrategy, ExtractJwt: ExtractJWT } = passportJWT;
+
+// define passport jwt strategy
+const opts = {};
+opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = `${process.env.JWT_SECRET}`;
+const passportJWTStrategy = new JWTStrategy(opts, function(jwtPayload, done) {
+  // retreive mail from jwt payload
+  const email = jwtPayload.email;
+  const user = User.findOne({email: email});
+  if (user) {
+    done(null, user);
+  } else {
+    done(null, false);
+  }
+});
+
+// Config passport
+module.exports = function(passport) {
+  // token strategy
+  passport.use(passportJWTStrategy);
+
+  // return configured passport
+  return passport;
+};
