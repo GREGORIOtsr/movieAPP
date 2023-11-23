29e744ae58af748f85d428daab996a1dbfcd787c
Alex Marquez <alexfmarquez@gmail.com>
2023-11-23 19:38:22 +0100

    button delete fav in my movies

--- src/public/addFavorite.js
+++ src/public/addFavorite.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   const addButton = document.querySelector('.button-favorite');
   addButton.addEventListener('click', async function() {
     const movieId = this.getAttribute('data-movie-id');
-    console.log(movieId)
       try {
           const response = await fetch('/api/addfavorite', {
               method: 'POST',
@@ -23,28 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   })
 
-  const removeButton = document.querySelector('.button-remove');
-  removeButton.addEventListener('click', async function() {
-    const movieId = this.getAttribute('data-movie-id');
-    console.log(movieId)
-      try {
-          const response = await fetch('/api/removefavorite', {
-              method: 'DELETE',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({movie_id: movieId }),
-          });
-          if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          const data = await response.json();
-          console.log('Success:', data);
-          alert("Movie removed from favorite!")
-      } catch (error) {
-          console.error('Error:', error);
-      }
-  })
-
+  
 
 })
--- src/public/getFavs.js
+++ src/public/getFavs.js
@@ -1,4 +1,3 @@
-// Este debe ser "pelisFavs", no "peliFav"
 const containerFavs = document.getElementById("pelisFavs");
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,21 +15,81 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             const data = await response.json();
-            console.log(data);
 
             containerFavs.innerHTML = "";
-            data.forEach(fav => {
+            for (const fav of data) { 
+                const responseDetail = await fetch(`/api/detail/${fav.movie_id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (!responseDetail.ok) {
+                    throw new Error(`HTTP error! status: ${responseDetail.status}`);
+                }
+
+                const dataDetail = await responseDetail.json();
                 const favElement = document.createElement('div');
                 favElement.classList.add('favorite-item'); 
-                favElement.innerHTML = 
-                `<div id=favcard>
-                    <p>ID de la Película: <a href="/detail/${fav.movie_id}">${fav.movie_id}</a></p>
-                </div>
+
+                favElement.innerHTML = `
+                    <div id="favcard">
+                        <img src="https://image.tmdb.org/t/p/w500${dataDetail.poster_path}" alt="Poster">
+                        <div id="textfav">
+                            <p>ID de la Película: <a href="/detail/${fav.movie_id}">${fav.movie_id}</a></p>
+                            <p>${dataDetail.title}</p>
+                            <p>${dataDetail.release_date}</p>
+                            <button id=botonRemoveFavs type="button" class="button-remove" data-movie-id=${fav.movie_id}> Remove from Favorites</button>
+                        </div>
+                    </div>
                 `;
                 containerFavs.appendChild(favElement);
-            })
+            }
         } catch (error) {
             console.error('Error:', error);
         }
     });
+
+    containerFavs.addEventListener('click', async function(event) {
+        if (event.target.classList.contains('button-remove')) {
+            const movieId = event.target.getAttribute('data-movie-id');
+    
+            try {
+                const response = await fetch(`/api/removefavorite/${movieId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                   
+                    body: JSON.stringify({ movie_id: movieId }),
+                });
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log('Success:', data);
+                alert("Movie removed from favorites!");
+    
+               
+                event.target.closest('.favorite-item').remove();
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
