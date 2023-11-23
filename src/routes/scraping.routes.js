f135cf9b8dc4affe7bd2ee74549578450ec92fd8
Maria <mariadiananaghiu09@gmail.com>
2023-11-23 13:54:49 +0100

    finish/scraper FilmAffinity, it finally works in detail view

--- src/routes/scraping.routes.js
+++ src/routes/scraping.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const scrapingController = require("../controllers/scraping.controller")
+const scrapingController = require("../controllers/controllers.views/detail.controller")
 const router = express.Router();
 
-router.get( "/scraperReviews", scrapingController);
+router.get( "/scraperReviews", scrapingController.getDetail);
 
 module.exports = router
